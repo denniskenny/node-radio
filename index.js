7b36089a65f2f33de771a78f68ebdaae2ad06ee7
Dennis Kenny <denniskenny@users.noreply.github.com>
2019-03-14 22:05:17 +0000

    added /mp3 endpoint that returns a list of files (not JSON)

--- index.js
+++ index.js
@@ -12,58 +12,70 @@ var router = new Router();
 
 router.get('/mp3', async function (ctx, next)  {
  
-    let fileList = '';
-
-    let createDB = (file, cb) => {
-        console.log(file.path);
-        var id3 = nodeID3.read(file.path);
-        console.log('id3 : ' + id3);
-        fileList += id3.artist + ' - ' + id3.title + '\n';
-        cb(null, file);
-    };   
-
-    let writeit =  () => {
-        console.log('done');
-        ctx.body = 'scanning files...' +  fileList;
-    }
-    let failit =  () => {
-        console.log('failed');
-        ctx.body = 'failed';
-    }
-
-    let vinyl = (resolve, reject) => {
-        return new Promise((resolve, reject) => {
-            vfs.src([testFolder + '**/*.mp3'], { read: false })
-            .pipe(map(createDB))
-            .on('end', () => {resolve();  ctx.body =  fileList;} )          
-        });
-     }     
-
-     await vinyl()
-
+    return fileList
 });
 
 app
-  .use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    ctx.set('X-Response-Time', `${ms}ms`);
-  })
-    // logger
-  .use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-  })
-  .use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    await next();
-  })
-  .use(router.routes())
-  .use(router.allowedMethods())
+    .use(async (ctx, next) => {
+        const start = Date.now();
+        await next();
+        const ms = Date.now() - start;
+        ctx.set('X-Response-Time', `${ms}ms`);
+        console.log('mp3 route called')
+        ctx.body =  fileList;         
+    })
+        // logger
+    .use(async (ctx, next) => {
+        const start = Date.now();
+        await next();
+        const ms = Date.now() - start;
+        console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+    })
+    .use(async (ctx, next) => {
+        ctx.set('Access-Control-Allow-Origin', '*');
+        ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+        await next();
+    })
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+let fileList = '';
+
+let createDB = (file, cb) => {
+    let id3 = nodeID3.read(file.path)
+    let newFile = ''
+    if(id3 === 'false' || !id3) { 
+        newFile = file.path.match(/([^\\]*)(?=\.)/, '')[1]
+    }
+    else {
+        newFile = id3.artist + ' - ' + id3.title + '\n'
+    }
+    console.log('id3 : ' + id3 + ' result: ' + newFile)
+    fileList += newFile
+    
+    cb(null, file)
+};   
+
+let writeit =  () => {
+    console.log('done');
+    ctx.body = 'scanning files...' +  fileList;
+}
+let failit =  () => {
+    console.log('failed');
+    ctx.body = 'failed';
+}
+
+let vinyl = (resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        vfs.src([testFolder + '**/*.mp3'], { read: false })
+        .pipe(map(createDB))
+        .on('end', () => {
+            resolve();
+        })          
+    });
+    }     
+
+vinyl(writeit, failit)
 
 app.listen(8000);
