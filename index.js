f9f0d8fa2a636af82b6e254e9af33442a7ab4aca
Dennis Kenny <denniskenny@users.noreply.github.com>
2019-04-05 16:40:00 +0100

    added mp3 streaming, http://localhost:8000/mp3?fileName=<filename>mp3&range=0

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require("koa")
 const Router = require("koa-router")
 const map = require("map-stream")
 const vfs = require("vinyl-fs")
+const fs = require("fs")
 const nodeID3 = require("node-id3")
 const testFolder = "./tests/"
 const app = new Koa()
@@ -58,12 +59,50 @@ router.get("/init", async (ctx, next) => {
   await next()
 })
 
+router.get("/list", async (ctx, next) => {
+  console.log("get list")
+  const start = Date.now()
+  const ms = Date.now() - start
+  ctx.body = fileList
+  ctx.set("X-Response-Time", `${ms}ms`)
+})
+
 router.get("/mp3", async (ctx, next) => {
-  console.log("get mp3s")
   const start = Date.now()
   const ms = Date.now() - start
   ctx.set("X-Response-Time", `${ms}ms`)
-  ctx.body = fileList
+  const Buffer = require("buffer").Buffer
+  console.log("get mp3 stream")
+
+  const filePath = testFolder + ctx.query.fileName
+  const range = ctx.query.range
+
+  stat = fs.statSync(filePath)
+  const fileSize = stat.size
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+    const chunksize = end - start + 1
+    const readStream = fs.createReadStream(filePath, { start, end })
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "audio/mpeg"
+    }
+    ctx.response.header = head
+    ctx.body = readStream
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "audio/mpeg"
+    }
+    const readStream = fs.createReadStream(filePath)
+    ctx.response.header = head
+    ctx.body = readStream
+  }
 })
 
 app
